8e42c91eb47522bf84d6be2cc376cbfbe282c8f1
Bintang Angelica Silalahi <114979582+bintangangelicas@users.noreply.github.com>
2024-03-05 13:27:21 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,24 +3,27 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
+import ssl 
 sns.set(style='dark')
 
+ssl._create_default_https_context = ssl._create_unverified_context 
+                                                                                   
 #Judul Dashboard
 st.title('Dashboard Penyewaan Sepeda')
 
 #Baca Dataset
 @st.cache_data 
 def load_data():
-    data = pd.read_csv('https://raw.githubusercontent.com/bintangangelicas/Analisis-Data-ML-Bangkit/96ea2a88a69f7873bcd44a9dfb30620fce1e5678/day.csv')
+    data = pd.read_csv('https://raw.githubusercontent.com/bintangangelicas/Analisis-Data-ML-Bangkit/main/day.csv',delimiter=',')
     return data
 
 def load_data1():
-    data1 = pd.read_csv('https://raw.githubusercontent.com/bintangangelicas/Analisis-Data-ML-Bangkit/96ea2a88a69f7873bcd44a9dfb30620fce1e5678/hour.csv')
+    data1 = pd.read_csv('https://raw.githubusercontent.com/bintangangelicas/Analisis-Data-ML-Bangkit/main/hour.csv')
     return data1
 
 st.subheader('Rata - Rata Penyewaan Berdasarkan Cuaca')
 
-jumlah_pinjam = load_data.groupby('weathersit')['cnt'].mean()
+jumlah_pinjam = load_data().groupby('weathersit')['cnt'].mean()
 
 plt.bar(jumlah_pinjam.index, jumlah_pinjam.values, color='green')
 
@@ -31,7 +34,7 @@ plt.ylabel('Mean Penyewaan')
 st.pyplot()
 
 
-jumlah_pinjam = load_data1.groupby('hr')['cnt'].mean()
+jumlah_pinjam = load_data1().groupby('hr')['cnt'].mean()
 
 plt.bar(jumlah_pinjam.index, jumlah_pinjam.values, color='brown')
 
@@ -39,6 +42,4 @@ plt.title('Rata - Rata Penyewaan Berdasarkan Jam')
 plt.xlabel('Waktu')
 plt.ylabel('Mean Penyewaan')
 
-plt.show()
-
-
+st.pyplot()
