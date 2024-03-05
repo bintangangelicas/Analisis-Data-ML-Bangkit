82e46536b3ce8d22a305502caab37020a2e00a7f
Bintang Angelica Silalahi <114979582+bintangangelicas@users.noreply.github.com>
2024-03-05 13:35:14 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -32,6 +32,18 @@ plt.ylabel('Mean Penyewaan')
 
 st.pyplot()
 
+st.markdown('''**Conclusion Pertanyaan 1 (Apakah ada musim/cuaca tertentu angka peminjaman/penyewaan sepeda meningkat? Jika ada, musim apa?)**
+
+1: Clear, Few clouds, Partly cloudy, Partly cloudy
+
+2: Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist
+
+3: Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds
+
+4: Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog
+
+Angka 1-3 merepresentasikan cuaca, di mana telah dijelaskan keterangan dari angka-angka tersebut.
+Dapat disimpulkan bahwa pada cuaca (1) di mana cuaca bersih dan berawan angka penyewaan sepeda meningkat lalu diikuti oleh cuaca (2) dan (3). Sedangkan di cuaca (4) tidak terdapat record peminjaman sepeda sama sekali.''')
 
 jumlah_pinjam = load_data1().groupby('hr')['cnt'].mean()
 
